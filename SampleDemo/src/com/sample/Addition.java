8eafdb0d0d6afc25b191a2ca4361c9f401998ba8
anki01-comd <119093113+anki01-comd@users.noreply.github.com>
2025-03-30 23:29:33 +0530

    Print the numbers and theire addition

--- SampleDemo/src/com/sample/Addition.java
+++ SampleDemo/src/com/sample/Addition.java
@@ -6,7 +6,10 @@ public class Addition {
 		int a=10;
 		int b=20;
 		int add=a+b;
-	System.out.println("Addition of two numbers>>"+add);	
+	
+	System.out.println("Value of a>>"+a);
+	System.out.println("Value of b>>"+b);
+	System.out.println("a"+"+"+"b"+"="+add);
 	}
 
 }
