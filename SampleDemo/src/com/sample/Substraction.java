3193da1806f722e3003d0316abc0286b4df60b06
Ankita <ankitawankhade0108@gmail.com>
2025-03-31 00:00:38 +0530

    Subtraction Of Two Numbers

---
+++ SampleDemo/src/com/sample/Substraction.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int add=a+b;
+	
+	System.out.println("Substraction of two numbers>>");
+	System.out.println("a"+"-"+"b"+"="+add);
+	}
+
+}
